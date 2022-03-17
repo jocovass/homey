9f412c122b5fe4c94fb8142c1adbd7ad1c634acd
JozsefVass <joco.udv@gmail.com>
2022-03-17 21:13:36 +0000

    refactor: household controllers with catchAsync

---
+++ packages/server/src/controllers/householdControllers.ts
@@ -0,0 +1,182 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { Household, ShoppingListItem } from '../models/householdModel';
+import { AppError } from '../utils/appError';
+import { catchAsync } from '../utils/catchAsync';
+
+export const createHousehold = catchAsync(
+    async (
+        req: Request<undefined, Promise<void>, { name: string }>,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const { name } = req.body;
+        const currentUser = req.user;
+        if (currentUser) {
+            if (currentUser.household?.householdRef) {
+                return next(
+                    new AppError(
+                        'A user can be member of one household at a time.',
+                        400,
+                    ),
+                );
+            }
+            // try to create the household if the name is used throw reject the request
+            const household = await Household.create({
+                name,
+                members: [currentUser._id],
+            });
+
+            currentUser.household = {
+                householdRef: household._id,
+                joined: new Date(),
+                role: 'owner',
+            };
+
+            const updatedUser = await currentUser.save();
+            res.status(201).json({
+                data: {
+                    user: updatedUser,
+                    household,
+                },
+            });
+        }
+    },
+);
+
+export const getHousehold = catchAsync(
+    async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+        const household = await Household.findById(id).populate({
+            path: 'members',
+            match: { _id: { $ne: req.user?._id } },
+        });
+
+        res.status(200).json({
+            data: {
+                household,
+            },
+        });
+    },
+);
+
+export const createShoppingList = catchAsync(
+    async (
+        req: Request<{ householdId: string }, Promise<void>, { title: string }>,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const { title } = req.body;
+        const { householdId } = req.params;
+        const household = await Household.findByIdAndUpdate(
+            householdId,
+            { $push: { shoppingList: { title } } },
+            {
+                new: true,
+            },
+        );
+
+        res.status(201).json({
+            data: {
+                household,
+            },
+        });
+    },
+);
+
+export const addShoppingListItem = catchAsync(
+    async (
+        req: Request<
+            { householdId: string; shoppinglistId: string },
+            Promise<void>,
+            ShoppingListItem
+        >,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const { label, amount, unit } = req.body;
+        const { householdId, shoppinglistId } = req.params;
+        const household = await Household.findOneAndUpdate(
+            {
+                _id: householdId,
+                shoppingList: { $elemMatch: { _id: shoppinglistId } },
+            },
+            {
+                $push: {
+                    'shoppingList.$.list': {
+                        label,
+                        amount,
+                        unit,
+                    },
+                },
+            },
+            {
+                new: true,
+            },
+        );
+
+        res.status(201).json({
+            data: {
+                household,
+            },
+        });
+    },
+);
+
+export const deleteShoppingListItem = catchAsync(
+    async (
+        req: Request<{
+            householdId: string;
+            shoppinglistId: string;
+            listId: string;
+        }>,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const { householdId, shoppinglistId, listId } = req.params;
+        const household = await Household.findOneAndUpdate(
+            {
+                _id: householdId,
+                shoppingList: { $elemMatch: { _id: shoppinglistId } },
+            },
+            {
+                $pull: {
+                    'shoppingList.$.list': {
+                        _id: listId,
+                    },
+                },
+            },
+            {
+                new: true,
+            },
+        );
+
+        res.status(204).json({
+            data: {
+                household,
+            },
+        });
+    },
+);
+
+export const deleteShoppingList = catchAsync(
+    async (
+        req: Request<{ householdId: string; shoppinglistId: string }>,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const { householdId, shoppinglistId } = req.params;
+        const household = await Household.findByIdAndUpdate(
+            householdId,
+            { $pull: { shoppingList: { _id: shoppinglistId } } },
+            {
+                new: true,
+            },
+        );
+        res.status(204).json({
+            data: {
+                household,
+            },
+        });
+    },
+);
--- packages/server/src/routes/householdRoutes.ts
+++ packages/server/src/routes/householdRoutes.ts
@@ -1,197 +1,31 @@
 import { Router } from 'express';
-import { Household } from '../models/householdModel';
-import { authMiddelware } from './userRoutes';
-import { ShoppingListItem } from '../models/householdModel';
+
+import { authMiddelware } from '../controllers/authControlles';
+import {
+    addShoppingListItem,
+    createHousehold,
+    createShoppingList,
+    deleteShoppingList,
+    deleteShoppingListItem,
+    getHousehold,
+} from '../controllers/householdControllers';
 
 export const householdRouter = Router();
 
 householdRouter.use(authMiddelware);
 
-householdRouter.post('/create', async (req, res, next) => {
-    try {
-        const { name }: { name: string } = req.body;
-        // req.user should have the current user data
-        const currentUser = req.user;
-        if (currentUser) {
-            if (currentUser.household?.householdRef) {
-                return next({
-                    statusCode: 400,
-                    message: 'A user can be member of one household at a time.',
-                });
-            }
-            // try to create the household if the name is used throw reject the request
-            const household = await Household.create({
-                name,
-                members: [currentUser._id],
-            });
-
-            currentUser.household = {
-                householdRef: household._id,
-                joined: new Date(),
-                role: 'owner',
-            };
-
-            const updatedUser = await currentUser.save();
-            return res.status(201).json({
-                data: {
-                    user: updatedUser,
-                    household,
-                },
-            });
-        }
-    } catch (error: any) {
-        return next({
-            statusCode: 500,
-            error: error.message,
-        });
-    }
-});
-
-householdRouter.get('/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const household = await Household.findById(id).populate({
-            path: 'members',
-            match: { _id: { $ne: req.user?._id } },
-        });
-
-        return res.status(200).json({
-            data: {
-                household,
-            },
-        });
-    } catch (error: any) {
-        return next({
-            statusCode: 500,
-            message: error.message,
-        });
-    }
-});
-
-householdRouter.post(
-    '/:householdId/shoppinglist/create',
-    async (req, res, next) => {
-        try {
-            const { title } = req.body;
-            const { householdId } = req.params;
-            const household = await Household.findByIdAndUpdate(
-                householdId,
-                { $push: { shoppingList: { title } } },
-                {
-                    new: true,
-                },
-            );
-            return res.status(201).json({
-                data: {
-                    household,
-                },
-            });
-        } catch (error: any) {
-            return next({
-                statusCode: 500,
-                message: error.message,
-            });
-        }
-    },
-);
-
+householdRouter.post('/create', createHousehold);
+householdRouter.get('/:id', getHousehold);
+householdRouter.post('/:householdId/shoppinglist/create', createShoppingList);
 householdRouter.post(
     '/:householdId/shoppinglist/:shoppinglistId/add',
-    async (req, res, next) => {
-        try {
-            const { label, amount, unit }: ShoppingListItem = req.body;
-            const { householdId, shoppinglistId } = req.params;
-            const household = await Household.findOneAndUpdate(
-                {
-                    _id: householdId,
-                    shoppingList: { $elemMatch: { _id: shoppinglistId } },
-                },
-                {
-                    $push: {
-                        'shoppingList.$.list': {
-                            label,
-                            amount,
-                            unit,
-                        },
-                    },
-                },
-                {
-                    new: true,
-                },
-            );
-
-            return res.status(201).json({
-                data: {
-                    household,
-                },
-            });
-        } catch (error: any) {
-            return next({
-                statusCode: 500,
-                message: error.message,
-            });
-        }
-    },
+    addShoppingListItem,
 );
-
 householdRouter.delete(
     '/:householdId/shoppinglist/:shoppinglistId/remove/:listId',
-    async (req, res, next) => {
-        try {
-            const { householdId, shoppinglistId, listId } = req.params;
-            const household = await Household.findOneAndUpdate(
-                {
-                    _id: householdId,
-                    shoppingList: { $elemMatch: { _id: shoppinglistId } },
-                },
-                {
-                    $pull: {
-                        'shoppingList.$.list': {
-                            _id: listId,
-                        },
-                    },
-                },
-                {
-                    new: true,
-                },
-            );
-
-            return res.status(204).json({
-                data: {
-                    household,
-                },
-            });
-        } catch (error: any) {
-            return next({
-                statusCode: 500,
-                message: error.message,
-            });
-        }
-    },
+    deleteShoppingListItem,
 );
-
 householdRouter.delete(
     '/:householdId/shoppinglist/:shoppinglistId',
-    async (req, res, next) => {
-        try {
-            const { householdId, shoppinglistId } = req.params;
-            const household = await Household.findByIdAndUpdate(
-                householdId,
-                { $pull: { shoppingList: { _id: shoppinglistId } } },
-                {
-                    new: true,
-                },
-            );
-            return res.status(204).json({
-                data: {
-                    household,
-                },
-            });
-        } catch (error: any) {
-            return next({
-                statusCode: 500,
-                message: error.message,
-            });
-        }
-    },
+    deleteShoppingList,
 );
